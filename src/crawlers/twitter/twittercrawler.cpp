d69850b038edcc75b54e29ddea58e9f6c0965af7
Luis Aguirre <Luis Aguirre@localhost>
2013-11-28 12:25:09 -0400

    Adding the trie structure

---
+++ src/crawlers/twitter/twittercrawler.cpp
@@ -0,0 +1,8 @@
+#include "twittercrawler.h"
+
+SocialInformationList TwitterCrawler::collect(Location location, float radio)
+{
+	SocialInformationList collectedInformation;
+
+	return collectedInformation;
+}
