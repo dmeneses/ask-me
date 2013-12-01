6917f90824eb92493fc942d427a0af1fe475e923
Daniela Meneses <daniela11290@gmail.com>
2013-12-01 01:09:10 -0400

    Adding stemming and matching logic to textpreprocessor.

--- src/textpreprocessor/textpreprocessor.cpp
+++ src/textpreprocessor/textpreprocessor.cpp
@@ -1,26 +1,30 @@
 #include "textpreprocessor.h"
-#include "textcleaner.h"
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
 TextPreprocessor::TextPreprocessor()
 {
+    cleaner_ = new TextCleaner();
+    stemmer_ = new Stemmer("spanish");
+    matcher_ = new Matcher(stemmer_);
 }
 
 vector<Tweet> TextPreprocessor::process(vector<Tweet> tweets, string toFind)
 {
-    vector<Tweet> textProcessed;
-    vector<Tweet>::iterator it;
-    TextCleaner* cleaner = new TextCleaner();
-    
-    for(it = tweets.begin(); it != tweets.end(); it++)
+    vector<Tweet> results;
+    vector<Tweet>::iterator tweetIterator;
+    string wordToFind = stemmer_->stem(toFind);
+    for (tweetIterator = tweets.begin(); tweetIterator != tweets.end(); tweetIterator++)
     {
-        string textcleaned = cleaner->clean(it->sentence);
-        //text minning
-        //Matches
-        
-        textProcessed.push_back(Tweet(textcleaned, it->latitude, it->longitude));
+        string textcleaned = cleaner_->clean(tweetIterator->sentence);
+        std::vector<std::string> splittedTweet;
+        boost::split(splittedTweet, textcleaned, boost::is_any_of("\t "));
+        if(matcher_->match(splittedTweet, wordToFind)){
+            results.push_back(*tweetIterator);
+        }
     }
-    
-    return textProcessed;
+
+    return results;
 }
+
