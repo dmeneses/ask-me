f53d7a26b73f9f175b7bafe1d364c32f6ce20d28
dann1 <dann1@ubuntu>
2013-11-30 19:19:55 -0800

    Adding full test with full input file containing input word and expected word after stemming.

--- test/stemmer/stemmertests.cpp
+++ test/stemmer/stemmertests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "stemmer/stemmer.h"
+#include <fstream>
 
 TEST(Stemmer, SimpleWord)
 {
@@ -15,4 +16,29 @@ TEST(Stemmer, WordWithAccents)
     std::string expectedWord = "abarrot";
     std::string stemmedWord = stemmer.stem("abarrotó");
     ASSERT_EQ(expectedWord, stemmedWord);
+}
+
+
+TEST(Stemmer, FullTests)
+{
+    Stemmer stemmer("spanish");
+    std::string inputWord = "";
+    std::string expectedStemmedWord = "";
+    std::string stemmedWord = "";
+    std::ifstream inputFile;
+    std::ifstream outputFile;
+
+    inputFile.open("resources/stem.in");
+    outputFile.open("resources/stem.out");
+
+    while (!inputFile.eof())
+    {
+        getline(inputFile, inputWord);
+        getline(outputFile, expectedStemmedWord);
+        stemmedWord = stemmer.stem(inputWord);
+        EXPECT_STREQ(expectedStemmedWord.c_str(), stemmedWord.c_str());
+    }
+
+    inputFile.close();
+    outputFile.close();
 }
