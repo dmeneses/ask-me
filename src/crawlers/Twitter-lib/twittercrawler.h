f167a64271d132ed14cbfaacabf338374c69d982
jorge <jorge@Botas>
2013-12-05 11:17:35 -0400

    Test twitter crawler

---
+++ src/crawlers/Twitter-lib/twittercrawler.h
@@ -0,0 +1,34 @@
+#ifndef _TWITTERCRAWLER_H
+#define _TWITTERCRAWLER_H
+
+#include "crawler.h"
+#include <string>
+
+struct BearerToken
+{
+    BearerToken(const std::string token, const std::string type): token_(token), type_(type){}
+    std::string token_;
+    std::string type_;
+};
+
+class TwitterCrawler : public Crawler
+{
+public:
+    TwitterCrawler();
+    ~TwitterCrawler();
+    SocialInformationList collect(Location location, float radio);
+    
+private:
+    void connect();
+    SocialInformationList search(Location location, float radio);
+    SocialInformationList parse(const char* jsonFile);
+    
+    const std::string encodeRFC1738(const std::string str);
+    static std::size_t writeRequestData(void *ptr, std::size_t size, std::size_t nmemb, void *data);
+    void setBearerToken(const std::string& json);
+    
+private:
+    BearerToken* token_;
+};
+
+#endif //_TWITTERCRAWLER_H
