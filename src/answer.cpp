7e639fbef4607191b418095f93a4e2503291a8fd
jorge <jorge@Botas>
2013-12-11 13:00:53 -0400

    Create the answer class

---
+++ src/answer.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   answer.cpp
+ * Author: jorge
+ * 
+ * Created on December 11, 2013, 12:50 PM
+ */
+
+#include "answer.h"
+
+std::vector<Result> Answer::ask(const std::string word, const Location location, const float radius, Crawler& crawler)
+{
+    std::vector<Result> result;
+    
+    return result;
+}
+
