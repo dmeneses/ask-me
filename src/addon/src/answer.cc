8991f0d7a29a9614a43c7177a09af717e38fab4e
jorge <jorge@Botas>
2013-12-12 12:27:38 -0400

    Implement a wrapper for answer class

--- src/addon/src/answer.cc
+++ src/addon/src/answer.cc
@@ -1,9 +1,10 @@
 #include "answer.h"
 #include "../../textpreprocessor/textpreprocessor.h"
+#include "../../answer.h"
 
 using namespace v8;
 
-void Answer::Init(v8::Handle<v8::Object> target)
+void AnswerJS::Init(v8::Handle<v8::Object> target)
 {
 	// Prepare constructor template
     Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
@@ -17,9 +18,44 @@ void Answer::Init(v8::Handle<v8::Object> target)
   target->Set(String::NewSymbol("Answer"), constructor);
 }
 
-v8::Handle<v8::Value> Answer::New(const v8::Arguments& args)
+v8::Handle<v8::Value> AnswerJS::New(const v8::Arguments& args)
 {
 }
-v8::Handle<v8::Value> Answer::Reply(const v8::Arguments& args)
+v8::Handle<v8::Value> AnswerJS::Reply(const v8::Arguments& args)
 {
+	HandleScope scope;
+	if(!args[0]->IsString() && !args[1]->IsNumber() && !args[2]->IsNumber() && !args[3]->IsNumber())
+		return scope.Close(Array::New(0));
+
+	Answer answer;
+	Location location;
+	String::Utf8Value value(args[0]->ToString());
+	std::string word = *value;
+	location.latitude_ = Handle<Number>::Cast(args[1])->Value();
+	location.longitude_ = Handle<Number>::Cast(args[2])->Value();
+	float radio = Handle<Number>::Cast(args[3])->Value();
+
+	std::vector<Result> results = answer.ask(word, location, radio, TWITTER);	
+
+	return scope.Close(ConvertVectorToArrayJS(results));
 }
+
+v8::Handle<v8::Array> ConvertVectorToArrayJS(std::vector<Result>& results)
+{
+	HandleScope scope;
+	Handle<Array> messages = Array::New(results.size());
+	
+	for(int i = 0; i < results.size(); i++)
+	{
+		SocialInformation info = results.at(i).information;
+		Local<Object> jsobject = Object::New();
+		jsobject->Set(String::NewSymbol("message"), String::New(info.message_.c_str()));
+		jsobject->Set(String::NewSymbol("latitude"), Number::New(info.location_.latitude_));
+		jsobject->Set(String::NewSymbol("longitude"), Number::New(info.location_.longitude_));
+		jsobject->Set(String::NewSymbol("source"), String::New(info.source_.c_str()));
+		messages->Set(i, jsobject);
+	}
+	
+	return scope.Close(messages);
+}
+
--- src/addon/src/answer.h
+++ src/addon/src/answer.h
@@ -1,15 +1,17 @@
-#ifndef ANSWER_H
-#define ANSWER_H
+#ifndef ANSWERJS_H
+#define ANSWERJS_H
 
 #include <node.h>
 #include <vector>
+#include "textpreprocessor.h"
 
-class Answer : public node::ObjectWrap {
+class AnswerJS : public node::ObjectWrap {
  public:
 	static void Init(v8::Handle<v8::Object> target);
  private:
 	static v8::Handle<v8::Value> New(const v8::Arguments& args);
 	static v8::Handle<v8::Value> Reply(const v8::Arguments& args);
+	static v8::Handle<v8::Array> ConvertVectorToArrayJS(std::vector<Result>& results);
 };
 
 #endif
