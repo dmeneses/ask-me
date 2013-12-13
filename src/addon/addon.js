65e8fad86f1bb62cf4f7ecb9bef5f8bfeddc8974
jorge <jorge@Botas>
2013-12-13 10:54:43 -0400

    Fix js compilation

--- src/addon/addon.js
+++ src/addon/addon.js
@@ -1,5 +1,15 @@
-var addon = require('./build/Release/addon');
+var addon = require('./build/Release/addon.node');
+
+console.log("Loaded addon");
 
 var answer = new addon.Answer();
 
-var array[] = answer.reply("Botas", 12312, 123123, 1);
+console.log("Created answer object");
+
+var array = answer.reply("Botas", 12312, 123123, 1);
+
+for(var i = 0; i < array.length; i++)
+{
+	var obj = array[i];
+	console.log(obj.message);
+}
