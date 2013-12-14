a8ec014214d86097a8c5e56959c18643a1b04529
jorge <jorge@Botas>
2013-12-14 08:03:14 -0400

    Link boost and jsoncpp with addon

--- src/addon/addon.js
+++ src/addon/addon.js
@@ -6,7 +6,7 @@ var answer = new addon.Answer();
 
 console.log("Created answer object");
 
-var array = answer.reply("Botas", 12312, 123123, 1);
+var array = answer.reply("Trabajando", -17.365978, -66.175486, 1);
 
 for(var i = 0; i < array.length; i++)
 {
