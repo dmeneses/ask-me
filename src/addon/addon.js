5aec3a6f718c0725cc61073f6a9ea2b6ea4d88f3
jorge <jorge@Botas>
2013-12-16 10:59:27 -0400

    Fix parse params to float

--- src/addon/addon.js
+++ src/addon/addon.js
@@ -6,7 +6,9 @@ var answer = new addon.Answer();
 
 console.log("Created answer object");
 
-var array = answer.reply("Trabajando", -17.365978, -66.175486, 1);
+var array = answer.reply("de", -17.365978, -66.175486, 1);
+
+console.log("Size: " + array.length);
 
 for(var i = 0; i < array.length; i++)
 {
