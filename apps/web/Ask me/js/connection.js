7926be3c645ffeac4734898f76388e5aa30ed5f4
jorge <jorge@Botas>
2013-12-19 10:54:22 -0400

    show information found

--- apps/web/Ask me/js/connection.js
+++ apps/web/Ask me/js/connection.js
@@ -1,17 +1,38 @@
 function connect()
 {
-	$.ajax({
-    url: 'http://localhost:3000/askme',
-    type: 'GET',
-    dataType: 'jsonp',
-    data: 'word=de&latitude=-17.365978&longitude=-66.175486&radio=1',
-    
-    success: function(data, xhr, status) {
-        alert('Hello world ' + data.Name);
-    },
+    var word = document.getElementById("word").value;
+    var url = format("word={0}&latitude=-17.365978&longitude=-66.175486&radio=1", word);
 
-    error: function(xhr, status) {
-        alert(xhr.status + " " + xhr.statusText);
+    $.ajax({
+        url: 'http://localhost:3000/askme',
+        type: 'GET',
+        dataType: 'jsonp',
+        data: '',
+
+        success: function(data, xhr, status) {
+            console.log('Connection success');
+            var array = $.parseJSON(data);
+            var items = new Array();
+            array.forEach(function(item){
+                items.push("<li>" + item.message + "</li>");
+            });
+            
+            $("ul").html(items.join(" "));
+        },
+
+        error: function(xhr, status) {
+            alert(xhr.status + " " + xhr.statusText);
+        }
+    });
+}
+
+function format() {
+    var s = arguments[0];
+
+    for (var i = 0; i < arguments.length - 1; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");             
+        s = s.replace(reg, arguments[i + 1]);
     }
-	});
+
+    return s;
 }
