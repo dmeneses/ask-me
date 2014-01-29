dec17ecdfc8628970b23448c9d1ab584faea3408
Daniela Meneses <daniela11290@gmail.com>
2014-01-29 13:56:39 -0400

    fixing compilation error in twitter lib.

--- src/crawlers/Twitter-lib/utils.h
+++ src/crawlers/Twitter-lib/utils.h
@@ -14,56 +14,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct iostring {
-  char *ptr;
-  size_t len;
+struct iostring
+{
+    char *ptr;
+    size_t len;
 };
 
-void init_string(struct iostring *s) {
-  s->len = 0;
-  s->ptr = (char*)malloc(s->len+1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "malloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  s->ptr[0] = '\0';
-}
+void init_string(struct iostring *s);
 
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct iostring *s)
-{
-  size_t new_len = s->len + size*nmemb;
-  s->ptr = (char*)realloc(s->ptr, new_len+1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "realloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->ptr+s->len, ptr, size*nmemb);
-  s->ptr[new_len] = '\0';
-  s->len = new_len;
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct iostring *s);
 
-  return size*nmemb;
-}
-
-std::string stringFormat(const std::string fmt, ...) {
-    int size = 100;
-    std::string str;
-    va_list ap;
-    while (1) {
-        str.resize(size);
-        va_start(ap, fmt);
-        int n = vsnprintf((char *)str.c_str(), size, fmt.c_str(), ap);
-        va_end(ap);
-        if (n > -1 && n < size) {
-            str.resize(n);
-            return str;
-        }
-        if (n > -1)
-            size = n + 1;
-        else
-            size *= 2;
-    }
-    return str;
-}
+std::string formatString(const std::string fmt, ...);
 
 #endif	/* UTILS_H */
 
