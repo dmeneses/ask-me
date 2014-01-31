5dca63d42815f68a40717ba56f42b3e504603de2
Daniela Meneses <daniela11290@gmail.com>
2014-01-30 23:17:21 -0400

    Update tweet list for every search. Setting off by default place search.

--- apps/web/AskMe_v2/public_html/js/map.js
+++ apps/web/AskMe_v2/public_html/js/map.js
@@ -1,66 +1,71 @@
 
 function showAdvancedSetting(box) {
-   	$("#setting-div").slideToggle("slow");
+    $("#setting-div").slideToggle("slow");
 }
 
 function setLocation(position) {
-	setMapPosition(position.coords.latitude, position.coords.longitude);
+    setMapPosition(position.coords.latitude, position.coords.longitude);
 }
 
 function setMapPosition(latitude, longitude)
 {
-	var lonLat = new OpenLayers.LonLat(longitude, latitude)
-	.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+    var lonLat = new OpenLayers.LonLat(longitude, latitude)
+            .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
 
-	map.setCenter(lonLat, 16);
+    map.setCenter(lonLat, 16);
 }
 
 function loadMap() {
-	map = new OpenLayers.Map("map-div");
-	map.addLayer(new OpenLayers.Layer.OSM());
-	map.zoomToMaxExtent();
+    map = new OpenLayers.Map("map-div");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.zoomToMaxExtent();
 
-   	markers = new OpenLayers.Layer.Markers("Markers");
-	map.addLayer(markers);
+    markers = new OpenLayers.Layer.Markers("Markers");
+    map.addLayer(markers);
 
-	actualPosition = new OpenLayers.Marker(new OpenLayers.LonLat(-98, 35));
-	markers.addMarker(actualPosition);
+    actualPosition = new OpenLayers.Marker(new OpenLayers.LonLat(-98, 35));
+    markers.addMarker(actualPosition);
 
-	map.events.register("click", map, function(e) {
-   		actualPosition.moveTo(e.xy);
-   	});
+    map.events.register("click", map, function(e) {
+        actualPosition.moveTo(e.xy);
+    });
 
-	getLocation(setLocation);
+    getLocation(setLocation);
 }
 
 function loadSocialInfo(socialInfoList)
 {
-	console.log('Loading social information list');
+    console.log('Loading social information list');
+    var tweetList = document.getElementById("tweetList").innerHTML;
 
-	for(var i = 0; i < socialInfoList.length; i++) {
-		addMarker(socialInfoList[i]);
-	}
+    for (var i = 0; i < socialInfoList.length; i++) {
+        addMarker(socialInfoList[i]);
+        tweetList += '<li class="list-group-item">' + socialInfoList[i].message + 
+                '<span class="glyphicon glyphicon-thumbs-up"></span></li>';
+    }
+    
+    document.getElementById("tweetList").innerHTML = tweetList;
 }
 
 function addMarker(socialInformation)
 {
-	var lonLat = new OpenLayers.LonLat(socialInformation.longitude, socialInformation.latitude)
-      .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
-	var marker = new OpenLayers.Marker(lonLat);
-    marker.events.register("click", marker, function(e){
-    	popup = new OpenLayers.Popup.FramedCloud("tweet", marker.lonlat, 
-    		new OpenLayers.Size(200, 200), socialInformation.message, null, true);
-    	map.addPopup(popup);
-    }); 
+    var lonLat = new OpenLayers.LonLat(socialInformation.longitude, socialInformation.latitude)
+            .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+    var marker = new OpenLayers.Marker(lonLat);
+    marker.events.register("click", marker, function(e) {
+        popup = new OpenLayers.Popup.FramedCloud("tweet", marker.lonlat,
+                new OpenLayers.Size(200, 200), socialInformation.message, null, true);
+        map.addPopup(popup);
+    });
     markers.addMarker(marker);
-    console.log('Social info message : ' + socialInformation.message);    
+    console.log('Social info message : ' + socialInformation.message);
 }
 
 function clearMarkers()
 {
-	actualPosition = new OpenLayers.Marker(actualPosition.lonlat);
-	markers.clearMarkers();
-	markers.addMarker(actualPosition);
+    actualPosition = new OpenLayers.Marker(actualPosition.lonlat);
+    markers.clearMarkers();
+    markers.addMarker(actualPosition);
 }
 
 loadMap();
